e22f4936aafe91dda9ec548a8cd737e92210be29
AnnBalias <annbalias@gmail.com>
2025-01-28 15:08:18 +0200

    forbes-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 // components
-import BlogCard from './components/BlogCard/BlogCard';
-import Container from './components/Container/Container';
-import CryptoHistory from './components/CryptoHistory/CryptoHistory';
-import ForbesList from './components/ForbesList/ForbesList';
-import Heading from './components/Heading/Heading';
 import Section from './components/Section/Section';
+import Container from './components/Container/Container';
+import Heading from './components/Heading/Heading';
+import BlogCard from './components/BlogCard/BlogCard';
 import Statistics from './components/Statistics/Statistics';
+import ForbesList from './components/ForbesList/ForbesList';
+import CryptoHistory from './components/CryptoHistory/CryptoHistory';
 // data
 import article from './data/article.json';
 import stats from './data/stats.json';
+import forbes from './data/forbes.json';
+
 
 export const App = () => {
   
@@ -30,7 +32,7 @@ export const App = () => {
         <Statistics title="Main Statistics" stats={stats} />
 
         <Heading title="Task 3 Forbes list" top bottom />
-        <ForbesList />
+        <ForbesList list={forbes} />
 
         <Heading title="Task 4 Crypto history" top bottom />
         <CryptoHistory />
